f4c3c4b2e5441ef0e6031e8c2e0bcfc899410a61
Christian Scholtysik <christianscholtysik@gmail.com>
2024-06-10 20:52:29 +0200

    add functionality for all enclosures and animals

--- ZooAdministration/src/main.ts
+++ ZooAdministration/src/main.ts
@@ -1,7 +1,38 @@
 import Animal from "./Animal";
 import EnclosureId from "./EnclosureId";
 import Continent from "./Continent";
-import Lion from "./animalClasses";
+import {
+  Lion,
+  Elephant,
+  Bear,
+  Monkey,
+  Dolphin,
+  Snake,
+  Turtle,
+  Crocodile,
+  Lizard,
+  Parrot,
+  Eagle,
+  Macaw,
+  Fish,
+  Trout,
+  Shark,
+  Octopus,
+  Squid,
+  Frog,
+  Alligator,
+  Swan,
+  Duck,
+  Clownfish,
+  Tiger,
+  Rhinoceros,
+  Cockatoo,
+  Pufferfish,
+  Panda,
+  Camel,
+  Toucan,
+  Flamingo,
+} from "./animalClasses";
 
 //* Elemente holen
 
@@ -30,6 +61,9 @@ const createAnimalButton = document.getElementById(
 ) as HTMLInputElement;
 
 const savannahEnclosure = document.getElementById("savannah") as HTMLElement;
+const jungleEnclosure = document.getElementById("jungle") as HTMLElement;
+const reptileEnclosure = document.getElementById("reptileHouse") as HTMLElement;
+const aquariumEnclosure = document.getElementById("aquarium") as HTMLElement;
 
 const allZooAnimals: Animal[] = [];
 const savannahAnimals: Animal[] = [];
@@ -49,6 +83,156 @@ function createAnimal(
     case "Lion":
       return new Lion(name, yearOfBirth, continent, specialNeeds, enclosureId);
       break;
+    case "Elephant":
+      return new Elephant(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Bear":
+      return new Bear(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Monkey":
+      return new Monkey(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Dolphin":
+      return new Dolphin(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Lion":
+      return new Lion(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Snake":
+      return new Snake(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Turtle":
+      return new Turtle(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Crocodile":
+      return new Crocodile(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Lizard":
+      return new Lizard(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Parrot":
+      return new Parrot(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Eagle":
+      return new Eagle(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Macaw":
+      return new Macaw(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Fish":
+      return new Fish(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Trout":
+      return new Trout(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Shark":
+      return new Shark(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Octopus":
+      return new Octopus(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Squid":
+      return new Squid(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Frog":
+      return new Frog(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Alligator":
+      return new Alligator(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Swan":
+      return new Swan(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Duck":
+      return new Duck(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Clownfish":
+      return new Clownfish(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Tiger":
+      return new Tiger(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Rhinoceros":
+      return new Rhinoceros(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Cockatoo":
+      return new Cockatoo(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Pufferfish":
+      return new Pufferfish(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Panda":
+      return new Panda(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Camel":
+      return new Camel(name, yearOfBirth, continent, specialNeeds, enclosureId);
+    case "Toucan":
+      return new Toucan(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
+    case "Flamingo":
+      return new Flamingo(
+        name,
+        yearOfBirth,
+        continent,
+        specialNeeds,
+        enclosureId
+      );
     default:
       break;
   }
@@ -83,6 +267,102 @@ function displayAnimalInEnclosure() {
         savannahAnimals.splice(index, 1);
         displayAnimalInEnclosure();
         console.log("SavannahHabitat after delete: ", savannahAnimals);
+      });
+    });
+  }
+
+  if (jungleEnclosure && jungleAnimals) {
+    jungleEnclosure.innerHTML = "";
+    jungleAnimals.forEach((animal: Animal, index) => {
+      const emojiDiv = document.createElement("div");
+      emojiDiv.className = "Emoji tooltip";
+      const emoji = document.createElement("div");
+      emoji.innerHTML = animal.emoji;
+      const currentYear = new Date().getFullYear();
+      const age = currentYear - animal.yearOfBirth;
+      //* Tooltipp
+      const tooltip = document.createElement("div");
+      tooltip.className = "tooltiptext";
+      tooltip.innerHTML = `
+        Name: ${animal.name}<br>
+       Age: ${age}<br>
+        Origin: ${animal.continent}<br>
+       Special Needs: ${animal.specialNeeds}
+       `;
+
+      emojiDiv.appendChild(emoji);
+      emojiDiv.appendChild(tooltip);
+
+      jungleEnclosure.appendChild(emojiDiv);
+      //* delete by dblclick
+      emojiDiv.addEventListener("dblclick", () => {
+        jungleAnimals.splice(index, 1);
+        displayAnimalInEnclosure();
+        console.log("JungleHabitat after delete: ", jungleAnimals);
+      });
+    });
+  }
+
+  if (reptileEnclosure && reptileHouseAnimals) {
+    reptileEnclosure.innerHTML = "";
+    reptileHouseAnimals.forEach((animal: Animal, index) => {
+      const emojiDiv = document.createElement("div");
+      emojiDiv.className = "Emoji tooltip";
+      const emoji = document.createElement("div");
+      emoji.innerHTML = animal.emoji;
+      const currentYear = new Date().getFullYear();
+      const age = currentYear - animal.yearOfBirth;
+      //* Tooltipp
+      const tooltip = document.createElement("div");
+      tooltip.className = "tooltiptext";
+      tooltip.innerHTML = `
+        Name: ${animal.name}<br>
+       Age: ${age}<br>
+        Origin: ${animal.continent}<br>
+       Special Needs: ${animal.specialNeeds}
+       `;
+
+      emojiDiv.appendChild(emoji);
+      emojiDiv.appendChild(tooltip);
+
+      reptileEnclosure.appendChild(emojiDiv);
+      //* delete by dblclick
+      emojiDiv.addEventListener("dblclick", () => {
+        reptileHouseAnimals.splice(index, 1);
+        displayAnimalInEnclosure();
+        console.log("ReptileHabitat after delete: ", reptileHouseAnimals);
+      });
+    });
+  }
+
+  if (aquariumEnclosure && aquariumAnimals) {
+    aquariumEnclosure.innerHTML = "";
+    aquariumAnimals.forEach((animal: Animal, index) => {
+      const emojiDiv = document.createElement("div");
+      emojiDiv.className = "Emoji tooltip";
+      const emoji = document.createElement("div");
+      emoji.innerHTML = animal.emoji;
+      const currentYear = new Date().getFullYear();
+      const age = currentYear - animal.yearOfBirth;
+      //* Tooltipp
+      const tooltip = document.createElement("div");
+      tooltip.className = "tooltiptext";
+      tooltip.innerHTML = `
+        Name: ${animal.name}<br>
+       Age: ${age}<br>
+        Origin: ${animal.continent}<br>
+       Special Needs: ${animal.specialNeeds}
+       `;
+
+      emojiDiv.appendChild(emoji);
+      emojiDiv.appendChild(tooltip);
+
+      aquariumEnclosure.appendChild(emojiDiv);
+      //* delete by dblclick
+      emojiDiv.addEventListener("dblclick", () => {
+        aquariumAnimals.splice(index, 1);
+        displayAnimalInEnclosure();
+        console.log("AquariumHabitat after delete: ", aquariumAnimals);
       });
     });
   }
@@ -132,11 +412,11 @@ createAnimalButton?.addEventListener("click", (event: Event) => {
         console.log("Jungle Habitat", jungleAnimals);
       }
       if (animal?.enclosureId === EnclosureId.ReptileHouse) {
-        savannahAnimals.push(animal);
+        reptileHouseAnimals.push(animal);
         console.log("Reptile Habitat", reptileHouseAnimals);
       }
       if (animal?.enclosureId === EnclosureId.AquaticHabitat) {
-        savannahAnimals.push(animal);
+        aquariumAnimals.push(animal);
         console.log("aquarium Habitat", aquariumAnimals);
       }
     }
